8637628441bbbfb2844162441150d2666ecedcc6
Hamit Mizrak <hamitmizrak@gmail.com>
2024-02-13 13:19:29 +0300

    CONST,LET,VAR, hoisting,console.error,warn,infoilog and alert, w,ndow.alert

---
+++ _5_Javascript/javascript.js
@@ -0,0 +1,58 @@
+// alert("External JS")
+// window.alert("External JS")
+/* 
+multiple Comment1
+multiple Comment2
+multiple Comment3
+*/
+
+// LOG
+// console.log("Loglama");
+
+// // WARNING
+// console.warn("Warning");
+
+// // INFO
+// console.info("Info");
+
+// // ERROR
+// console.error("Error");
+
+////////////////////////////////////////////////////////
+// VAR (Global)
+var kelime="Js Öğreniyorum JS";
+console.log(kelime);
+//alert(kelime)
+
+// hoisting
+kelime44="Kelimeler";
+var kelime44;
+
+var sayi=44;
+console.log(sayi);
+
+var sayi=44.23;
+console.log(sayi);
+
+var karar=true;
+console.log(karar);
+
+// LET
+let kelime2="Math2"
+console.log(kelime2);
+
+let kelime3="Math3"
+console.log(kelime3);
+kelime3="Math33333"
+console.log(kelime3);
+
+// CONST
+const kelime4="Math4"
+console.log(kelime4);
+
+const kelime5="Math5"
+console.log(kelime5);
+
+// yazamazsınız.
+// kelime5="Math5"
+// console.log(kelime5);
