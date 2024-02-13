f598831c789d010c7d21c94584c9c65353aa071b
Hamit Mizrak <hamitmizrak@gmail.com>
2024-02-13 16:19:37 +0300

    Sayısal İşlemler

--- _5_Javascript/javascript.js
+++ _5_Javascript/javascript.js
@@ -159,22 +159,90 @@ console.log(data);
 
 /////////////////////////////////////////////////////////
 /// ESCAPE CHARACTER 
-let escape1="1.alan '2.alan";
-console.log(escape1);
+// let escape1="1.alan '2.alan";
+// console.log(escape1);
 
-escape1="1.alan \"2.alan";
-console.log(escape1);
+// escape1="1.alan \"2.alan";
+// console.log(escape1);
 
-escape1='1.alan \'2.alan';
-console.log(escape1);
+// escape1='1.alan \'2.alan';
+// console.log(escape1);
 
-escape1='1.alan \"2.alan';
-console.log(escape1);
+// escape1='1.alan \"2.alan';
+// console.log(escape1);
 
 
-escape1='1.satır \n\t2.satır\u00A9';
-console.log(escape1);
+// escape1='1.satır \n\t2.satır\u00A9';
+// console.log(escape1);
 
-// null 
-let data3=null;
-console.log(data3);
+// // null 
+// let data3=null;
+// console.log(data3);
+
+/////////////////////////////////////////////////////////
+/// SAYISAL İŞLEMLER
+let number1=55.44;
+//document.writeln(number1);
+// NaN , isNaN
+console.log(number1);
+console.log(typeof number1);
+
+//Cast
+console.log(Number(number1));
+console.log(parseInt(number1));
+console.log(parseFloat(number1));
+
+// Bilimsel Gösterim
+let normal=1300000;
+console.log(normal);
+
+let bilimsel=13E+5;
+console.log(bilimsel);
+bilimsel=13E-5;
+console.log(bilimsel);
+
+const number2=1453.1234;
+
+console.log(number2.toExponential(1));
+
+// Verilen Bir tabanda 10'luk tabana çevirmek
+let binary=0b01010101111;
+console.log(binary);
+
+let octal=0o76511;
+console.log(octal);
+
+let decimal=123456;
+console.log(decimal);
+
+let hexadecimal=0xfbc12;
+console.log(hexadecimal);
+
+// 10 tabandan istediğimiz diğer tabanalar çevirmek
+let number3=4444;
+// Binary (2)
+let binary3=number3.toString(2);
+console.log(binary3);
+
+// Octal (2)
+let octal3=number3.toString(8);
+console.log(octal3);
+
+// Decimal(10)
+let decimal3=number3.toString(10);
+console.log(decimal3);
+
+// Decimal(10)
+let hexadecimal3=number3.toString(16);
+console.log(hexadecimal3);
+
+
+const number4=1234.56749;
+console.log(number4.toFixed(3));
+console.log(number4.toPrecision(3));
+
+
+
+
+/////////////////////////////////////////////////////////
+/// METİNSEL İŞLEMLER
