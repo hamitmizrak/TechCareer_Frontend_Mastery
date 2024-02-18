78bb3138954c1591b1d38ec09b481a6c700731d9
Hamit Mizrak <hamitmizrak@gmail.com>
2024-02-18 15:14:24 +0300

    Stream

---
+++ _6_NodeJs/_4_stream/_1_CreateReadStream.js
@@ -0,0 +1,42 @@
+// Stream: Büyük verileri parça parça işlemek için kullanılır.
+
+// File 
+const fs = require('fs');
+
+// Dosya Yolu
+const dosyaTamYolu='./istockphoto.mp4';
+
+// Read
+const readStreamData=fs.createReadStream(dosyaTamYolu);
+
+// DataMount
+let dataMount=0;
+
+// Veri 
+readStreamData.on('data', function(chunk){
+    //console.log("Veri Okundu: "+chunk);
+    console.log(`Veri Büyüklüğü: ${chunk.length}`);
+    dataMount+=chunk.length;
+    console.log(`Veri Okundu: ${chunk}`);
+    console.log(chunk);
+}); //end readStreamData
+
+// End
+readStreamData.on('end', ()=>{
+    //console.log("Veri Okundu: "+chunk);
+    console.log(`Veri Okunması tamamdır `);
+    console.log(`Veri Toplam Byte Büyüklüğü: ${dataMount}`);
+}); //end readStreamData
+
+// Close
+readStreamData.on('close', ()=>{
+    console.log(`Veri Kapatıldı `);
+}); //end readStreamData
+
+
+// End
+readStreamData.on('error', (err)=>{
+    console.error(`Hata meydana geldi: ${err}`);
+}); //end readStreamData
+
+// https://www.istockphoto.com/tr/video/time-lapse-low-angle-of-tall-corporate-buildings-skyscraper-gm1421465990-467087403
